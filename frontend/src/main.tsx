4c84d7574285419660f5109315994c09e2e5452a
bartoszb0 <bartoszb2020@gmail.com>
2026-07-14 17:32:57 +0200

    update imports for ThemeProvider and App components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
+import { ThemeProvider } from "@/components/common/theme-provider"
+import App from "@/App.tsx"
 import "./index.css"
-import App from "./App.tsx"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
