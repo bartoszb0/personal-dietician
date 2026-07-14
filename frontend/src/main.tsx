3d5f894ec6ad49627a08a41583829fc5328516ee
bartoszb0 <bartoszb2020@gmail.com>
2026-07-14 22:23:24 +0200

    refactor: integrate QueryClientProvider and Toaster into main render

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,21 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import { ThemeProvider } from "@/components/common/theme-provider"
+import { Toaster } from "@/components/ui/sonner"
 import App from "@/App.tsx"
 import "./index.css"
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <App />
+        <Toaster />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 )
