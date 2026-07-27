d1b5e3e46a1bedb7f993b6f7e57348041a97137e
bartoszb0 <bartoszb2020@gmail.com>
2026-07-27 19:18:08 +0200

    update routes in router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import Home from "@/pages/Home"
 import NotFound from "@/pages/NotFound"
+import { ProtectedRoute } from "./components/common/ProtectedRoute"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="*" element={<NotFound />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
