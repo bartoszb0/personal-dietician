8a0e4b0854db70b43edeb47d2e6816b3cc352456
bartoszb0 <bartoszb2020@gmail.com>
2026-07-27 21:35:01 +0200

    add create profile dto

--- backend/src/profile/dto/create-profile.dto.ts
+++ backend/src/profile/dto/create-profile.dto.ts
@@ -1,1 +1,33 @@
-export class CreateProfileDto {}
+import {
+  IsDateString,
+  IsEnum,
+  IsInt,
+  IsNumber,
+  Max,
+  Min,
+} from 'class-validator';
+import { ActivityLevel, Goal, Sex } from '../../../generated/prisma/enums';
+
+export class CreateProfileDto {
+  @IsEnum(Sex)
+  declare sex: Sex;
+
+  @IsDateString()
+  declare birthDate: string;
+
+  @IsInt()
+  @Min(100)
+  @Max(250)
+  declare heightCm: number;
+
+  @IsNumber()
+  @Min(30)
+  @Max(300)
+  declare weightKg: number;
+
+  @IsEnum(ActivityLevel)
+  declare activityLevel: ActivityLevel;
+
+  @IsEnum(Goal)
+  declare goal: Goal;
+}
