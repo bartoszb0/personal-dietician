51109039783c15dc5e15411909db53caa9d1e878
bartoszb0 <bartoszb2020@gmail.com>
2026-07-27 20:14:12 +0200

    change app styling, add font

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+
 import { ThemeProvider } from "@/components/common/theme-provider"
 import { Toaster } from "@/components/ui/sonner"
 import App from "@/App.tsx"
