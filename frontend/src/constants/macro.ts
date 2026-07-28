aba34dee6bae44a660c856e593de6b56c00ecbde
bartoszb0 <bartoszb2020@gmail.com>
2026-07-28 10:29:25 +0200

    add macro constants

---
+++ frontend/src/constants/macro.ts
@@ -0,0 +1,5 @@
+export const MACROS = [
+  { key: "proteinG", label: "Protein", kcalPerGram: 4 },
+  { key: "carbsG", label: "Carbs", kcalPerGram: 4 },
+  { key: "fatG", label: "Fat", kcalPerGram: 9 },
+] as const
