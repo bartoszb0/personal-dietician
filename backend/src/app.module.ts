2804458f0c4a8c3ab23d297f53d1b351279b61cf
bartoszb0 <bartoszb2020@gmail.com>
2026-07-28 12:00:47 +0200

    add stub meal resource

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { NutritionModule } from './nutrition/nutrition.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProfileModule } from './profile/profile.module';
+import { MealsModule } from './meals/meals.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { ProfileModule } from './profile/profile.module';
     AuthModule,
     ProfileModule,
     NutritionModule,
+    MealsModule,
   ],
   controllers: [],
   providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
