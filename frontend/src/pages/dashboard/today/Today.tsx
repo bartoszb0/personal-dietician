62c5014fef8d42513afba06f73f7d37e20fa4d78
bartoszb0 <bartoszb2020@gmail.com>
2026-07-28 11:26:52 +0200

    add query calls for getting current target and consumed nutrition

--- frontend/src/pages/dashboard/today/Today.tsx
+++ frontend/src/pages/dashboard/today/Today.tsx
@@ -1,4 +1,22 @@
+import {
+  getConsumedNutrition,
+  getCurrentNutritionTarget,
+} from "@/api/nutrition"
+import { useQuery } from "@tanstack/react-query"
+
 export default function Today() {
+  const { data: target } = useQuery({
+    queryKey: ["current-target"],
+    queryFn: () => getCurrentNutritionTarget(),
+  })
+
+  const { data: consumed } = useQuery({
+    queryKey: ["consumed-nutrition"],
+    queryFn: () => getConsumedNutrition(),
+  })
+
+  console.log(target, consumed)
+
   return (
     <div className="p-6">
       <h1 className="text-xl font-medium">Today</h1>
