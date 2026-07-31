5d66c54bd22bcfa6fa88f9c8a9885a18a214ec46
bartoszb0 <bartoszb2020@gmail.com>
2026-07-31 17:13:32 +0200

    add api call to update profile

--- frontend/src/api/profile.ts
+++ frontend/src/api/profile.ts
@@ -5,3 +5,9 @@ export async function getProfileData(): Promise<UserProfile> {
   const { data } = await api.get<UserProfile>("/profile")
   return data
 }
+
+// todo fix any
+export async function updateProfile(patch: any): Promise<any> {
+  const { data } = await api.patch<UserProfile>("/profile", patch)
+  return data
+}
